db3670f46becf4dabd2d7c046d4427202cdcd55d
marcoweb <marcocarvalho.web@hotmail.com>
2020-11-23 21:57:32 +0000

    Database

--- assets/sql/schema.mysql.sql
+++ assets/sql/schema.mysql.sql
@@ -26,3 +26,62 @@ CREATE TABLE tokens(
     revoked BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE Habilitacao (
+    habId int PRIMARY KEY AUTO_INCREMENT,
+    habNome varchar(250) NOT NULL, 
+    habOrgaoClasse varchar(250) NOT NULL
+);
+
+CREATE TABLE Profissional (
+    proId int PRIMARY KEY AUTO_INCREMENT,
+    proNome varchar(250) NOT NULL,
+    proDataNas date NOT NULL,
+    proTelefone char(14) NOT NULL, 
+    proCPF char(11) NOT NULL,
+    proRG varchar(250) NOT NULL,
+    proEndNumero int NOT NULL,           
+    proEndCidade varchar(250) NOT NULL,  
+    proEndLogradouro varchar(250) NOT NULL,
+    proEndCEP char(8) NOT NULL,
+    proFoto varchar(250) NOT NULL,
+    proEmail varchar(250) NOT NULL, 
+    proRegistroProfissional int NOT NULL,
+    proHabId int NOT NULL,
+    FOREIGN KEY (proHabId) REFERENCES Habilitacao(habId)   
+);
+
+CREATE TABLE Disponibilidade (
+    disId int PRIMARY KEY AUTO_INCREMENT, 
+    disInicio datetime NOT NULL, 
+    disFim datetime NOT NULL, 
+    disProId int NOT NULL, 
+    FOREIGN KEY (disProid) REFERENCES Profissional(proId)
+);
+
+CREATE TABLE Paciente (
+    pacid int PRIMARY KEY AUTO_INCREMENT,
+    pacNome varchar(250) NOT NULL,
+    pacDataNas date NOT NULL,
+    pacTelefone char(14) NOT NULL,
+    pacCPF char(11) NOT NULL,
+    pacRG varchar(250) NOT NULL,
+    pacEndNumero int NOT NULL,
+    pacEndCidade varchar(250) NOT NULL,
+    pacEndLogradouro varchar(250) NOT NULL,
+    pacEndCep char(8) NOT NULL,
+    pacFoto varchar(250) NOT NULL, 
+    pacEmail varchar(250) NOT NULL,
+    pacPlanoSaude  varchar(250) NOT NULL
+);
+
+CREATE TABLE Agendamento (
+    ageId int PRIMARY KEY AUTO_INCREMENT, 
+    ageData date NOT NULL, 
+    ageHorario time NOT NULL, 
+    ageObservacoes varchar(250) NOT NULL, 
+    agePacId int NOT NULL, 
+    ageProId int NOT NULL,
+    FOREIGN KEY (agePacId) REFERENCES Paciente (pacId),
+    FOREIGN KEY(ageProId) REFERENCES Profissional (proId)
+);
